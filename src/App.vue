4c509f564c9c8328cc92e984acb2ee5ddea2e594
Gene H <gshuh.mail@gmail.com>
2021-01-09 17:33:22 -0500

    Untangled JSON data from API for daily death count array

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="cbus-memorial" ref="app">
-    <img 
-    :src="cbus" alt="Columbus Skyline" width="100%" />
-    <div v-for="(star, i) in stars" :key="i"
-      :class="star.class"
-      :style="`left:${star.style.left}px; top:${star.style.top}px;`"></div>
-    <span>Total Deaths: {{totalDeaths}}</span>
-  </div>
+	<div id="cbus-memorial" ref="app">
+		<img :src="cbus" alt="Columbus Skyline" width="100%" />
+		<div
+			v-for="(star, i) in stars"
+			:key="i"
+			:class="star.class"
+			:style="`left:${star.style.left}px; top:${star.style.top}px;`"
+		></div>
+		<span>Total Deaths: {{ totalDeaths }}</span>
+	</div>
 </template>
 
 <script>
@@ -14,94 +16,131 @@ import axios from "axios";
 import cbus from "./assets/CBUS-memorial-photo.jpg";
 
 export default {
-  name: 'cbus-remembers',
-  data() {
-    return {
-      cbus: cbus,
-      dataURL: 'http://covid.maxheckel.me/api/counties/deaths?counties=',
-      counties: [
-        "Franklin",
-        "Delaware",
-        "Hocking",
-        "Fairfield",
-        "Knox",
-        "Licking",
-        "Montgomery",
-        "Madison",
-        "Marion",
-        "Perry",
-        "Pickaway",
-        "Union",
-      ],
-      deathJSON: [],
-      totalDeaths: 0,
-      newDeaths: 0,
-      stars: [],
-    };
-  },
-  methods: {
-    createStar(x, y, type) {
-      var elem =this.$el.createElement("div");
-      elem.setAttribute("class", type);
-      elem.setAttribute("style", "left:" + x + "px;top:" + y + "px;");
-      this.$el.getElementByName("cbus-memorial")[0].appendChild(elem);
-      return elem;
-    },
-    randomX() {
-      return Math.floor(Math.random() * this.skyWidth);
-    },
-    randomY() {
-      return Math.floor(Math.random() * this.skyHeight * .65);
-    }
-  },
-  computed: {
-    skyHeight() {
-      return this.$refs.app.clientHeight;
-    },
-    skyWidth() {
-      return this.$refs.app.clientWidth;
-    }
-  },
-  async created() {
-    // const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-    const requestedURL = this.dataURL+this.counties.join(",");
-    const response = await axios.get(
-      // proxyURL+
-      requestedURL);
+	name: "cbus-remembers",
+	data() {
+		return {
+			cbus: cbus,
+			dataURL: "http://covid.maxheckel.me/api/counties/deaths?counties=",
+			counties: [
+				"Franklin",
+				"Delaware",
+				"Hocking",
+				"Fairfield",
+				"Knox",
+				"Licking",
+				"Montgomery",
+				"Madison",
+				"Marion",
+				"Perry",
+				"Pickaway",
+				"Union"
+			],
+			totalDeaths: 0,
+			dailyDeathsByCounty: [],
+			stars: []
+		};
+	},
+	computed: {
+		dailyDeaths() {
+			const mergeDailyDeathsObj = {};
+			this.dailyDeathsByCounty.forEach(county => {
+				for (let [key, value] of Object.entries(county)) {
+					if (!mergeDailyDeathsObj[key]) {
+						mergeDailyDeathsObj[key] = value;
+					} else {
+						mergeDailyDeathsObj[key] += value;
+					}
+				}
+			});
 
-    this.totalDeaths = response.data.total;
-    
-    for (let i = 0; i < this.totalDeaths; i++) {
-      this.stars.push({
-        class: 'old-star',
-        style: {
-          left: this.randomX(),
-          top: this.randomY()
-        }
-      })
-    }
-  },
+			const result = [];
+			for (let [key, value] of Object.entries(mergeDailyDeathsObj)) {
+				result.push({
+					date: key,
+					count: value
+				});
+			}
+			return this.sortDaysAscending(result);
+		}
+	},
+	skyHeight() {
+		return this.$refs.app.clientHeight;
+	},
+	skyWidth() {
+		return this.$refs.app.clientWidth;
+	},
+	methods: {
+		countyReducer(dailyCountArray) {
+			if (dailyCountArray) {
+				return dailyCountArray.reduce((acc, day) => {
+					if (!acc[day.date]) {
+						acc[day.date] = day.count;
+					} else {
+						acc[day.date] = day.count;
+					}
+					return acc;
+				}, {});
+			}
+			return [];
+		},
+		createStar(x, y, type) {
+			var elem = this.$el.createElement("div");
+			elem.setAttribute("class", type);
+			elem.setAttribute("style", "left:" + x + "px;top:" + y + "px;");
+			this.$el.getElementByName("cbus-memorial")[0].appendChild(elem);
+			return elem;
+		},
+		randomX() {
+			return Math.floor(Math.random() * this.skyWidth);
+		},
+		randomY() {
+			return Math.floor(Math.random() * this.skyHeight * 0.65);
+		},
+		sortDaysAscending(array) {
+			return array.sort((a, b) => Date(a.date) - Date(b.date));
+		}
+	},
+	async created() {
+		const proxyURL = "https://cors-anywhere.herokuapp.com/";
+		const requestedURL = this.dataURL + this.counties.join(",");
+		const response = await axios.get(proxyURL + requestedURL);
+
+		this.totalDeaths = response.data.total;
+		this.dailyDeathsByCounty = response.data.counties.map(county => {
+			return this.countyReducer(county.days);
+		});
+
+		for (let i = 0; i < this.totalDeaths; i++) {
+			this.stars.push({
+				class: "old-star",
+				style: {
+					left: this.randomX(),
+					top: this.randomY()
+				}
+			});
+		}
+	}
 };
 </script>
 
 <style>
 .old-star {
-    position: absolute;
-    width: 2px;
-    height: 2px;
-    background: white;
-    z-index: 20;
+	position: absolute;
+	width: 2px;
+	height: 2px;
+	background: white;
+	z-index: 20;
 }
 
 .new-star {
-  position: relative;
-  width: 3px;
-  height: 3px;
-  background: yellow;
+	position: relative;
+	width: 3px;
+	height: 3px;
+	background: yellow;
 }
 
 div {
-  margin: auto;
-  width: 100%;
+	margin: auto;
+	width: 100%;
 }
 </style>
